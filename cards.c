968a6f63ee2b3bf8216aa5995ff24e407c1a486f
Alex Drover <alexfdrover@gmail.com>
2020-10-07 22:22:10 -0700

    cards.c

--- cards.c
+++ cards.c
@@ -7,34 +7,40 @@
 
 int main() {
   char card_name[3];
-  puts("Enter the card_name: ");
-  scanf("%2s", card_name);
-  
-  int val = 0;
-  switch(card_name[0]) {
-    case 'K': 
-      val = 10;
-      break;
-    case 'Q':
-      val = 10;
-      break;
-    case 'J':
-      val = 10;
-      break;
-    case 'A':
-      val = 11;
-      break;
-    default:
-      val = atoi(card_name);
+  int count = 0;
+  while (card_name[0] != 'X') {
+    puts("Enter the card_name: ");
+    scanf("%2s", card_name);
+
+    int val = 0;
+    switch(card_name[0]) {
+      case 'K': 
+        val = 10;
+        break;
+      case 'Q':
+        val = 10;
+        break;
+      case 'J':
+        val = 10;
+        break;
+      case 'A':
+        val = 11;
+        break;
+      case 'X':
+        continue;
+      default:
+        val = atoi(card_name);
+        if ((val < 1) || (val > 10)) {
+          puts("The card value is not within 1-10");
+          continue;
+        }
+    }
+    if ((val >= 3) && (val <= 6)) {
+      count += 1;
+    } else if (val == 10) {
+      count -= 1;
+    }
+    printf("Current count: %i\n", count);
   }
-
-  if ((val >= 3) && (val <= 6)) {
-    puts("Count has gone up");
-  } else if (val == 10) {
-    puts("Count has gone down");
-  }
-
-  // printf("The card value is: %i\n", val);
-
   return 0;
 }
